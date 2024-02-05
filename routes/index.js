9ca3be746b976951a6eb43f11623449551bed03f
Roy Lee <loyii91400@gmail.com>
2024-02-05 17:21:40 +1100

    fix: fix Jade lang

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Roy\'s Projects',
+  res.render('index', {
+    title: 'Roy\'s Projects',
     list: [
-      {name: "Original Smart Dots", link: "original-smart-dots"} ,
-      {name: "Smart dots (Under Construction)", link: "smart-dots" },
-      {name: "Platformer", link: "platformer"},
-      {name: "Chinese Song Association", link: "chinese-song-association"}
-  ]
+      { name: "Original Smart Dots", link: "original-smart-dots", title: "original smart dots" },
+      { name: "Smart dots (Under Construction)", link: "smart-dots", title: "smart dots" },
+      { name: "Platformer", link: "platformer", title: "platformer" },
+      { name: "Chinese Song Association", link: "chinese-song-association", title: "chinese song association" }
+    ]
   });
 });
 
--- routes/projects.js
+++ routes/projects.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/original-smart-dots', function(req, res, next) {
-  res.render('original-smart-dots', { title: 'Original Smart Dots' });
+  res.render('original-smart-dots', { title: 'Original-Smart-Dots' });
 });
 
 router.get('/smart-dots', function(req, res, next) {
-  res.render('smart-dots', { title: 'Smart dots' });
+  res.render('smart-dots', { title: 'Smart-dots' });
 });
 
 router.get('/platformer', function(req, res, next) {
@@ -15,7 +15,7 @@ router.get('/platformer', function(req, res, next) {
 });
 
 router.get('/chinese-song-association', function(req, res, next) {
-  res.render('chinese-song-association', { title: 'Chinese Song Association' });
+  res.render('chinese-song-association', { title: 'Chinese-Song-Association' });
 });
 
 
